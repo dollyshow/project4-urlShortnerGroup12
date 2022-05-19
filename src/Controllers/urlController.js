e89b0f2db05d32c009fe2bf8ba8e101c129b118d
Dolly <showdolly100@gmail.com>
2022-05-19 22:14:49 +0530

    project4

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -35,26 +35,23 @@ const createUrl = async (req, res) => {
         
  // destructure the longUrl from req.body.longUrl
         const longUrl = req.body.longUrl;
-// The API base Url endpoint
-         const baseUrl = 'http:localhost:3000'
+
         if(!longUrl) return res.status(400).send({status: false, message: "longUrl is required"})
-        if(!validUrl.isUri(baseUrl)){
-         return res.status(401).send({status: false, message: "Invalid baseUrl"});
- }
- // if valid, we create the url code
-        let urlCode = shortid.generate().toLowerCase();
-        if(!urlCode) return res.status(400).send({status: false, message: "invalid"})
+       
  // check long url if valid using the validUrl.isUri method
         if(validUrl.isUri(longUrl)){
-    
-                let url = await urlModel.findOne({longUrl : longUrl}).select({_id: 0, __v: 0});
+         let url = await urlModel.findOne({longUrl : longUrl}).select({_id: 0, __v: 0});
   // if url exist and return the respose
                 if(url){
  //using set to assign new key value pair in cache
         await SET_ASYNC(`${longUrl}`,JSON.stringify(url))
                     return res.status(200).send({status:true,message:"this longUrl is already present in db",data:url});
                 }else{
-    
+     // The API base Url endpoint
+ const baseUrl = 'http://localhost:3000'
+ // if valid, we create the url code
+        let urlCode = shortid.generate().toLowerCase();
+        if(!urlCode) return res.status(400).send({status: false, message: "invalid"})
  // join the generated urlcode to the baseurl
                     let shortUrl = baseUrl + "/" + urlCode;
                     url  = await urlModel.create({longUrl, shortUrl, urlCode});
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,4 +8,12 @@ router.post("/url/shorten", createUrl )
 router.get("/:urlCode", getUrl )
 
 
+// if api is invalid OR wrong URL
+router.all("/**", function (req, res) {
+    res.status(404).send({
+        status: false,
+        msg: "The api you request is not available"
+    })
+})
+
 module.exports = router;
